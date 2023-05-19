e0b5a67babadb548363bbf6856d01eb323c7b98e
ofri masad <ofri@deci.ai>
2023-05-19 17:17:26 +0300

    fix download

--- data/squad_translated/download.sh
+++ data/squad_translated/download.sh
@@ -2,6 +2,6 @@
 
 for lang in "ar" "de" "el" "es" "hi" "ru" "th" "tr" "vi" "zh" "cs" "fa" "fr" "iw" "nl" "ru" "sv"
 do
-  wget  https://translated-qa.s3.amazonaws.com/dev-v1.1-hf_${lang}.json -P $( dirname -- "$0"; )
-  wget  https://translated-qa.s3.amazonaws.com/train-v1.1-hf_${lang}.json -P $( dirname -- "$0"; )
+  wget  https://translated-qa.s3.amazonaws.com/dev-v1.1-hf_${lang}.json --no-check-certificate -P $( dirname -- "$0"; )
+  wget  https://translated-qa.s3.amazonaws.com/train-v1.1-hf_${lang}.json --no-check-certificate -P $( dirname -- "$0"; )
 done
--- data/xquad_Translated_train/download.sh
+++ data/xquad_Translated_train/download.sh
@@ -2,5 +2,5 @@
 
 for lang in "ar" "de" "el" "es" "hi" "ru" "th" "tr" "vi" "zh"
 do
-  wget  https://translated-qa.s3.amazonaws.com/squad.translate.train.en-${lang}-hf.json -P $( dirname -- "$0"; )
+  wget  https://translated-qa.s3.amazonaws.com/squad.translate.train.en-${lang}-hf.json --no-check-certificate -P $( dirname -- "$0"; )
 done
